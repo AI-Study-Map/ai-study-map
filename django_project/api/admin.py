11d7f1e13b5d7b9f8c1b8f012ed64c610b8c5cb9
s1F102102942 <s1F102102942@iniad.org>
2023-10-04 14:14:12 +0900

    DBのモデルを追加 概要はslackに

--- django_project/api/admin.py
+++ django_project/api/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from .models import Gpt_call, Question
+from .models import Gpt_call, Question, User, Map, Node, Edge
 
 admin.site.register(Gpt_call)
 admin.site.register(Question)
+admin.site.register(User)
+admin.site.register(Map)
+admin.site.register(Node)
+admin.site.register(Edge)
--- django_project/api/models.py
+++ django_project/api/models.py
@@ -7,3 +7,46 @@ class Gpt_call(models.Model):
 class Question(models.Model):
 
     body = models.TextField(blank=False, null=False)
+
+
+class User(models.Model):
+    user_id = models.CharField(max_length=255, primary_key=True)
+    last_name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    gender = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    birthdate = models.DateField()
+    level = models.PositiveIntegerField(default=0)
+    exp = models.PositiveIntegerField(default=0)
+
+class Map(models.Model):
+    map_id = models.CharField(max_length=255, primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    theme_name = models.CharField(max_length=255)
+    graph_structure = models.TextField(null=True)
+    achieve_percentage = models.PositiveIntegerField(default=0)
+    total_nodes = models.PositiveIntegerField(default=0)
+    cleared_nodes = models.PositiveIntegerField(default=0)
+
+class Node(models.Model):
+    node_id = models.CharField(max_length=255, primary_key=True)
+    map = models.ForeignKey(Map, on_delete=models.CASCADE)
+    x_coordinate = models.FloatField()
+    y_coordinate = models.FloatField()
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True)
+    example = models.TextField(null=True)
+    question = models.TextField(null=True)
+    question_a = models.TextField(null=True)
+    question_b = models.TextField(null=True)
+    question_c = models.TextField(null=True)
+    question_d = models.TextField(null=True)
+    true_answer = models.CharField(max_length=10, null=True)
+    is_cleared = models.BooleanField(default=False)
+    is_extra_node = models.BooleanField(default=False)
+
+class Edge(models.Model):
+    edge_id = models.CharField(max_length=255, primary_key=True)
+    map = models.ForeignKey(Map, on_delete=models.CASCADE)
+    parent_node = models.ForeignKey(Node, related_name='parent_node', on_delete=models.CASCADE)
+    child_node = models.ForeignKey(Node, related_name='child_node', on_delete=models.CASCADE)
