5a4b1fe9b1bb26b09bce2589e79400caa545eb1f
s1F102103303 <s1F102103303@iniad.org>
2023-10-11 15:38:15 +0900

    構造を作成

--- react_app/src/node/store.js
+++ react_app/src/node/store.js
@@ -18,7 +18,7 @@ import {
       {
         id: '1',
         type: 'mindmap',
-        data: { label: 'データ型' },
+        data: { label: '基本概念' },
         position: { x: 200, y: -70 },
         dragHandle: '.dragHandle',
         idd: 2,
@@ -26,7 +26,7 @@ import {
       {
         id: '2',
         type: 'mindmap',
-        data: { label: '演算子と制御フロー' },
+        data: { label: '関数とモジュール' },
         position: { x: 200, y: 100 },
         dragHandle: '.dragHandle',
         idd: 2,
@@ -34,19 +34,19 @@ import {
       {
         id: '3',
         type: 'mindmap',
-        data: { label: '関数とモジュール' },
+        data: { label: 'クラスとオブジェクト指向プログラミング (OOP)' },
         position: { x: -200, y: -70 },
         dragHandle: '.dragHandle',
         idd: 2,
       },
-      {
-        id: '4',
-        type: 'mindmap',
-        data: { label: 'クラスとオブジェクト' },
-        position: { x: -200, y: 100 },
-        dragHandle: '.dragHandle',
-        idd: 2,
-      },
+      // {
+      //   id: '4',
+      //   type: 'mindmap',
+      //   data: { label: 'クラスとオブジェクト' },
+      //   position: { x: -200, y: 100 },
+      //   dragHandle: '.dragHandle',
+      //   idd: 2,
+      // },
     ],
     edges: [
       {
@@ -148,6 +148,266 @@ import {
     nodeContent: '',
     nodeExample: '',
     setQuestionDetail: (title, content, example) => set({ nodeTitle: title, nodeContent: content, nodeExample: example }),
+    test: {
+      "name": "Python",
+      "children": [
+        {
+          "name": "基本概念",
+          "children": [
+            {
+              "name": "データ型と変数",
+              "children": [
+                {
+                  "name": "数値型",
+                  "children": [
+                    {
+                      "name": "整数",
+                      "children": [
+                        {
+                          "name": "算術演算",
+                          "children": []
+                        },
+                        {
+                          "name": "整数の範囲",
+                          "children": []
+                        }
+                      ]
+                    },
+                    {
+                      "name": "浮動小数点数",
+                      "children": [
+                        {
+                          "name": "精度",
+                          "children": []
+                        },
+                        {
+                          "name": "四則演算",
+                          "children": []
+                        }
+                      ]
+                    }
+                  ]
+                },
+                {
+                  "name": "文字列",
+                  "children": [
+                    {
+                      "name": "エスケープシーケンス",
+                      "children": []
+                    },
+                    {
+                      "name": "文字列メソッド",
+                      "children": []
+                    }
+                  ]
+                }
+              ]
+            },
+            {
+              "name": "制御構造",
+              "children": [
+                {
+                  "name": "条件分岐",
+                  "children": [
+                    {
+                      "name": "if文",
+                      "children": [
+                        {
+                          "name": "比較演算子",
+                          "children": []
+                        },
+                        {
+                          "name": "論理演算子",
+                          "children": []
+                        }
+                      ]
+                    },
+                    {
+                      "name": "switch-case代替",
+                      "children": [
+                        {
+                          "name": "辞書による代替",
+                          "children": []
+                        },
+                        {
+                          "name": "if-elif-elseによる代替",
+                          "children": []
+                        }
+                      ]
+                    }
+                  ]
+                },
+                {
+                  "name": "ループ",
+                  "children": [
+                    {
+                      "name": "forループ",
+                      "children": [
+                        {
+                          "name": "range関数",
+                          "children": []
+                        },
+                        {
+                          "name": "enumerate関数",
+                          "children": []
+                        }
+                      ]
+                    },
+                    {
+                      "name": "whileループ",
+                      "children": [
+                        {
+                          "name": "条件式",
+                          "children": []
+                        },
+                        {
+                          "name": "breakとcontinue",
+                          "children": []
+                        }
+                      ]
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "name": "関数とモジュール",
+          "children": [
+            {
+              "name": "関数定義",
+              "children": [
+                {
+                  "name": "引数と戻り値",
+                  "children": [
+                    {
+                      "name": "デフォルト引数",
+                      "children": []
+                    },
+                    {
+                      "name": "可変長引数",
+                      "children": []
+                    }
+                  ]
+                },
+                {
+                  "name": "スコープと名前空間",
+                  "children": [
+                    {
+                      "name": "ローカルスコープ",
+                      "children": []
+                    },
+                    {
+                      "name": "グローバルスコープ",
+                      "children": []
+                    }
+                  ]
+                }
+              ]
+            },
+            {
+              "name": "モジュールとパッケージ",
+              "children": [
+                {
+                  "name": "モジュールのインポート",
+                  "children": [
+                    {
+                      "name": "import文",
+                      "children": []
+                    },
+                    {
+                      "name": "from-import文",
+                      "children": []
+                    }
+                  ]
+                },
+                {
+                  "name": "パッケージ作成",
+                  "children": [
+                    {
+                      "name": "__init__.py",
+                      "children": []
+                    },
+                    {
+                      "name": "パッケージの構造",
+                      "children": []
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "name": "クラスとオブジェクト指向プログラミング (OOP)",
+          "children": [
+            {
+              "name": "クラス定義",
+              "children": [
+                {
+                  "name": "インスタンスメソッドと属性",
+                  "children": [
+                    {
+                      "name": "コンストラクタ",
+                      "children": []
+                    },
+                    {
+                      "name": "デストラクタ",
+                      "children": []
+                    }
+                  ]
+                },
+                {
+                  "name": "クラス変数とインスタンス変数",
+                  "children": [
+                    {
+                      "name": "クラス変数",
+                      "children": []
+                    },
+                    {
+                      "name": "インスタンス変数",
+                      "children": []
+                    }
+                  ]
+                }
+              ]
+            },
+            {
+              "name": "継承とポリモーフィズム",
+              "children": [
+                {
+                  "name": "基底クラスと派生クラス",
+                  "children": [
+                    {
+                      "name": "継承",
+                      "children": []
+                    },
+                    {
+                      "name": "オーバーライド",
+                      "children": []
+                    }
+                  ]
+                },
+                {
+                  "name": "抽象クラスとインターフェース",
+                  "children": [
+                    {
+                      "name": "抽象クラス",
+                      "children": []
+                    },
+                    {
+                      "name": "インターフェース",
+                      "children": []
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    },
   }));
   
   export default useStore;
