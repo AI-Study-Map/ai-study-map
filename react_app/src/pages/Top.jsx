5adbecd8365d5a7a1891fb2251905beab4799831
s1F102102942 <s1F102102942@iniad.org>
2023-10-29 17:48:54 +0900

    Top.jsx下書き

--- react_app/src/pages/Top.jsx
+++ react_app/src/pages/Top.jsx
@@ -1,11 +1,58 @@
 import NodeTreeMake from "../node/NodeTreeMake";
+import React from 'react';
+import styled from 'styled-components';
+
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 2rem;
+  background-color: #e9e9e9; // 適切な背景色を設定
+`;
+const LeftSection = styled.div`
+  background-color: #4caf50; // 適切な背景色を設定
+  padding: 1rem 2rem;
+  flex: 0.5;
+  h1 {
+    font-size: 1.5rem;
+    color: white;
+  }
+  p {
+    margin-top: 1rem;
+    color: white;
+  }
+`;
+const RightSection = styled.div`
+  background-color: white; // 適切な背景色を設定
+  padding: 1rem 2rem;
+  flex: 0.5;
+  border: 1px dashed #4caf50;
+  h2 {
+    font-size: 1.5rem;
+  }
+`;
+const Button = styled.button`
+  background-color: #4caf50;
+  color: white;
+  padding: 0.5rem 1rem;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
 
 function Top() {
   return (
     <>
-        <h1>Top</h1>
-        <hr />
+    <Container>
+      <LeftSection>
+        <h1>logo</h1>
+        <p>適切なテキスト内容をこちらに記述します。</p>
+      </LeftSection>
+      <RightSection>
         <NodeTreeMake />
+      </RightSection>
+    </Container>
     </>
   );
 }
